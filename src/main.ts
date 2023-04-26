017cf177639c849a551870f0971e202e784d5358
zhangheng <taoist_zh@163.com>
2023-04-26 09:43:08 +0800

    初始化swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //允许跨域
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  //设置全局前缀
+  app.setGlobalPrefix('api');
+  //swagger接口文档配置
+  const options = new DocumentBuilder()
+    .setTitle('实验室设备管理系统api')
+    .setDescription('实验室设备管理系统api')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
